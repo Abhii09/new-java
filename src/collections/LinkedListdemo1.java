42ccdbf26207ac3a36d0626fa789c2f8b0b8bac7
Abhii09 <achavan799@gmail.com>
2020-01-12 03:21:24 +0530

    linked list and vector

---
+++ src/collections/LinkedListdemo1.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.*;
+
+public class LinkedListdemo1 {
+
+	public static void main(String[] args) {
+		LinkedList<String> l1=new LinkedList<String>();
+		l1.add("i am");
+		l1.add("abhi");
+		l1.add("chavan");
+		l1.add("karad");
+		l1.set(0, "This is");
+		l1.add(0,"hii");
+		l1.removeLast();
+		l1.add("Null");
+		
+		System.out.println(l1);
+		
+		
+	}
+
+}
---
+++ src/collections/VectorDemoclass.java
@@ -0,0 +1,31 @@
+package collections;
+
+import java.util.*;
+
+public class VectorDemoclass {
+	public static void main(String[] args) {
+		Vector v=new Vector();
+		v.add("abhi");
+	for(int i=0;i<10;i++) {
+		v.addElement(i);
+	}
+	System.out.println(v);
+	System.out.println(v.capacity());
+	System.out.println(v.size());
+	
+	Vector v1=new Vector(10,5);
+	v1.add("2nd");v1.add("2nd");v1.add("2nd");v1.add("2nd");v1.add("2nd");v1.add("2nd");v1.add("2nd");
+	v1.addAll(v);
+
+	System.out.println(v1);
+	System.out.println(v1.capacity());
+	System.out.println(v1.size());
+	
+	}
+	
+	
+	
+	
+	
+
+}
